fa002945452437d081dae73f46ef09cc481d0d53
tylerhkmontana <tylerhkmontana@gmail.com>
2024-07-19 15:58:49 -0400

    added the title

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.scss';
+import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>Hyunbin Kim's Portfolio Website</Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
